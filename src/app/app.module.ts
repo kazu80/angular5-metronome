6e934544715a2a9e08f30ecd12c96b45b8aa9d6f
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-11-22 07:13:34 +0900

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {routing} from './app.routing';
 
 import {AppComponent} from './component/app/app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports     : [
+        BrowserModule,
+        routing
+    ],
+    providers   : [],
+    bootstrap   : [AppComponent],
+    schemas     : [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {
+}
