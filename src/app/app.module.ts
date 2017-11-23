7d980c690c35f20970eab166360adbcfb08ba9f8
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-11-24 07:42:35 +0900

    tempo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {routing} from './app.routing';
 
 import {AppComponent} from './component/app/app.component';
+import {TopComponent} from './component/metronome/top/top.component';
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        TopComponent
     ],
     imports     : [
         BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,11 @@
 import {RouterModule} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 // import {MainComponent} from './component/stop-watch/main/main.component';
-// import {TopComponent} from './component/metronome/top/top.component';
+import {TopComponent} from './component/metronome/top/top.component';
 // import {RecordingComponent} from './component/recording/recording/recording.component';
 
 export const routing: ModuleWithProviders = RouterModule.forRoot([
-//    {path: '', component: TopComponent},
+    {path: '', component: TopComponent},
 //    {path: 'stop-watch', component: MainComponent},
 //    {path: 'recording', component: RecordingComponent},
 ]);
