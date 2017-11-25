c69ed17970423ce2127eea1b9d8b53fc50964c36
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-11-25 09:04:12 +0900

    tempo components

--- src/app/component/metronome/top/top.component.ts
+++ src/app/component/metronome/top/top.component.ts
@@ -6,11 +6,26 @@ import {Component, OnInit} from '@angular/core';
     styleUrls  : ['./top.component.scss']
 })
 export class TopComponent implements OnInit {
+    param: number;
+    params: string;
 
     constructor() {
     }
 
     ngOnInit() {
+        this.param  = 100;
+        this.params = JSON.stringify({
+            min : 1,
+            max : 255,
+            step: 1,
+        });
+    }
+
+    change(val) {
+        const value = val.detail.value;
+        if (value) {
+            console.log(value);
+        }
     }
 
 }
