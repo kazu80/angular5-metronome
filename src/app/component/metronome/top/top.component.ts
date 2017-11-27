e89ff2eabd3ea80377ca3d90a681dd74939e0cc4
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-11-28 06:52:35 +0900

    volume components

--- src/app/component/metronome/top/top.component.ts
+++ src/app/component/metronome/top/top.component.ts
@@ -12,19 +12,21 @@ export class TopComponent implements OnInit {
     beats: string;
     sound: number;
     sounds: string;
+    volume: number;
+    volumes: string;
 
     constructor() {
     }
 
     ngOnInit() {
-        this.tempo  = 100;
-        this.tempos = JSON.stringify({
+        this.tempo   = 100;
+        this.tempos  = JSON.stringify({
             min : 1,
             max : 255,
             step: 1,
         });
-        this.beat   = 4;
-        this.beats  = JSON.stringify([
+        this.beat    = 4;
+        this.beats   = JSON.stringify([
             {id: 1, active: false},
             {id: 2, active: false},
             {id: 3, active: false},
@@ -34,12 +36,24 @@ export class TopComponent implements OnInit {
             {id: 7, active: false},
             {id: 8, active: false},
         ]);
-        this.sound  = 1;
-        this.sounds = JSON.stringify([
+        this.sound   = 1;
+        this.sounds  = JSON.stringify([
             {id: 1, file: '/assets/sound/s_01.mp3', active: false},
             {id: 2, file: '/assets/sound/s_02.mp3', active: false},
             {id: 3, file: '/assets/sound/s_03.mp3', active: false},
             {id: 4, file: '/assets/sound/s_04.mp3', active: false},
+        ]);
+        this.volume  = 5;
+        this.volumes = JSON.stringify([
+            {id: 1, active: false},
+            {id: 2, active: false},
+            {id: 3, active: false},
+            {id: 4, active: false},
+            {id: 5, active: false},
+            {id: 6, active: false},
+            {id: 7, active: false},
+            {id: 8, active: false},
+            {id: 9, active: false},
         ]);
     }
 
