8a6addf4d57a4e539d48203ec090608eff4b2b98
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-11-29 08:19:08 +0900

    refactoring play

--- src/app/component/metronome/top/top.component.ts
+++ src/app/component/metronome/top/top.component.ts
@@ -14,19 +14,20 @@ export class TopComponent implements OnInit {
     sounds: string;
     volume: number;
     volumes: string;
+    play_params: string;
 
     constructor() {
     }
 
     ngOnInit() {
-        this.tempo   = 100;
-        this.tempos  = JSON.stringify({
+        this.tempo       = 100;
+        this.tempos      = JSON.stringify({
             min : 1,
             max : 255,
             step: 1,
         });
-        this.beat    = 4;
-        this.beats   = JSON.stringify([
+        this.beat        = 4;
+        this.beats       = JSON.stringify([
             {id: 1, active: false},
             {id: 2, active: false},
             {id: 3, active: false},
@@ -37,14 +38,14 @@ export class TopComponent implements OnInit {
             {id: 8, active: false},
         ]);
         this.sound   = 1;
-        this.sounds  = JSON.stringify([
+        this.sounds      = JSON.stringify([
             {id: 1, file: '/assets/sound/s_01.mp3', active: false},
             {id: 2, file: '/assets/sound/s_02.mp3', active: false},
             {id: 3, file: '/assets/sound/s_03.mp3', active: false},
             {id: 4, file: '/assets/sound/s_04.mp3', active: false},
         ]);
-        this.volume  = 5;
-        this.volumes = JSON.stringify([
+        this.volume      = 5;
+        this.volumes     = JSON.stringify([
             {id: 1, active: false},
             {id: 2, active: false},
             {id: 3, active: false},
@@ -55,13 +56,48 @@ export class TopComponent implements OnInit {
             {id: 8, active: false},
             {id: 9, active: false},
         ]);
+        this.play_params = JSON.stringify([
+            {tempo: this.tempo, beat: this.beat, sound: this.sound, volume: this.volume}
+        ]);
     }
 
     changeTempo(val) {
         const value = val.detail.value;
         if (value) {
-            // console.log(value);
+            this.tempo       = value;
+            this.play_params = JSON.stringify([
+                {tempo: this.tempo, beat: this.beat, sound: this.sound, volume: this.volume}
+            ]);
         }
     }
 
+    changeBeat(val) {
+        const value = val.detail.value;
+        if (value) {
+            this.beat        = value;
+            this.play_params = JSON.stringify([
+                {tempo: this.tempo, beat: this.beat, sound: this.sound, volume: this.volume}
+            ]);
+        }
+    }
+
+    changeSound(val) {
+        const value = val.detail.value;
+        if (value) {
+            this.sound       = value;
+            this.play_params = JSON.stringify([
+                {tempo: this.tempo, beat: this.beat, sound: this.sound, volume: this.volume}
+            ]);
+        }
+    }
+
+    changeVolume(val) {
+        const value = val.detail.value;
+        if (value) {
+            this.volume      = value;
+            this.play_params = JSON.stringify([
+                {tempo: this.tempo, beat: this.beat, sound: this.sound, volume: this.volume}
+            ]);
+        }
+    }
 }
