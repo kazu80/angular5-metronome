6e01c2847fa2421419997ab268d520d5d83f5858
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-12-06 08:04:16 +0900

    create voice changer components

--- src/app/component/voice-changer/voice-changer.component.ts
+++ src/app/component/voice-changer/voice-changer.component.ts
@@ -6,11 +6,54 @@ import {Component, OnInit} from '@angular/core';
     styleUrls  : ['./voice-changer.component.css']
 })
 export class VoiceChangerComponent implements OnInit {
+    audioContext: AudioContext                      = new AudioContext();
+    audioSources: Array<MediaStreamAudioSourceNode> = [];
+    spectrumAudioAnalyser: AnalyserNode;
+    sonogramAudioAnalyser: AnalyserNode;
+
+    private spectrumFFTSize: number;
+    private spectrumSmoothing: number;
+    private sonogramFFTSize: number;
+    private sonogramSmoothing: number;
 
     constructor() {
+        this.spectrumFFTSize   = 128;
+        this.spectrumSmoothing = 0.8;
+        this.sonogramFFTSize   = 2048;
+        this.sonogramSmoothing = 0;
     }
 
     ngOnInit() {
+        this.initAudio();
+        // initProcessor();
+        // initSliders();
+        // initCanvas();
+
+        // window.requestAnimFrame(renderCanvas);
+    }
+
+    initAudio() {
+        console.log('initAudio');
+
+        if (!navigator.getUserMedia) {
+            alert('Your browser does not support the Media Stream API');
+        } else {
+            navigator.getUserMedia({audio: true, video: false},
+                (stream: MediaStream) => {
+                    this.audioSources[1] = this.audioContext.createMediaStreamSource(stream);
+                },
+                (error) => {
+                    alert('Unable to get the user media');
+                });
+        }
+
+        this.spectrumAudioAnalyser                       = this.audioContext.createAnalyser();
+        this.spectrumAudioAnalyser.fftSize               = this.spectrumFFTSize;
+        this.spectrumAudioAnalyser.smoothingTimeConstant = this.spectrumSmoothing;
+
+        this.sonogramAudioAnalyser                       = this.audioContext.createAnalyser();
+        this.sonogramAudioAnalyser.fftSize               = this.sonogramFFTSize;
+        this.sonogramAudioAnalyser.smoothingTimeConstant = this.sonogramSmoothing;
     }
 
 }
