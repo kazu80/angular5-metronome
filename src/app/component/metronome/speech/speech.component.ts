2be8db82bf9725cd381a44013eae1adf92ea63a9
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-12-14 09:34:26 +0900

    hello metronome

--- src/app/component/metronome/speech/speech.component.ts
+++ src/app/component/metronome/speech/speech.component.ts
@@ -7,11 +7,16 @@ import {Component, ElementRef, OnInit} from '@angular/core';
 })
 export class SpeechComponent implements OnInit {
     private _el: HTMLElement;
-            speechResult: string;
 
+    speechResult: string;
+    sound_file: string;
+    isPlaySound: boolean;
 
     constructor(el: ElementRef) {
         this._el = el.nativeElement;
+
+        this.sound_file  = '/assets/audio/beep.wav';
+        this.isPlaySound = false;
     }
 
     ngOnInit() {
@@ -21,12 +26,30 @@ export class SpeechComponent implements OnInit {
             if (voiceEvent.detail.isFinal) {
                 this.speechResult = voiceEvent.detail.speechResult;
 
+                // response sound
+                this.isPlaySound = true;
+
                 switch (voiceEvent.detail.speechResult) {
-                    case 'hello metronome':
-                        console.log('ok!!');
+                    case 'okay metronome':
+                        break;
+
+                    default:
                         break;
                 }
 
+            }
+        });
+
+        voiceButton.addEventListener('onStateChange', (latest: any) => {
+            switch (latest.detail.newValue) {
+                case 'listening':
+                    this.speechResult = '';
+                    this.isPlaySound  = false;
+                    break;
+                case 'user-input':
+                    break;
+                case 'idle':
+                    break;
             }
         });
     }
